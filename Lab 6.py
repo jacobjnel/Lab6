5d3f306428dee18cc62df37d7fffa5f8c4daf816
omeraviram <148989775+omeraviram@users.noreply.github.com>
2023-10-25 12:24:24 -0400

    Update Lab 6.py

--- Lab 6.py
+++ Lab 6.py
@@ -4,3 +4,29 @@ def decode_password(encoded_password):
     for char in encoded_password:
         decoded_password += chr(ord(char) - 3)
     return decoded_password
+
+def encode_pass(password):
+    result = ""
+    for char in password:
+        if char.isdigit():
+            new_digit = str(int(char) + 3)
+            result += new_digit
+        else:
+            result += char
+    return result
+
+
+while True:
+
+    print("Menu")
+    print("------------")
+    print("1. Encode\n2. Decode\n3. Quit\n")
+    choice = (int(input("Please enter an option: ")))
+
+    if choice == 1:
+        password = (input("Please enter your password to encode: "))
+        new_password = encode_pass(password)
+        print("Your password has been encoded and stored!\n")
+
+    if choice == 3:
+        break
